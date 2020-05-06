20cc2baf4e93dc03e6bdc45c3b4f8703a9030364
Joseph T Colonel <josephtcolonel@gmail.com>
2020-05-06 15:15:31 +0100

    Add files via upload

---
+++ timbre-interp-gui.py
@@ -0,0 +1,418 @@
+from Tkinter import *
+import numpy as np
+import pandas as pd
+import os
+import librosa
+import soundfile as sf
+import argparse
+import pyaudio
+import numpy as np
+from keras.layers import Input, Dense, LeakyReLU
+from keras.models import Model, load_model
+import tensorflow as tf 
+from scipy import signal
+import time
+import sys
+import scipy, pylab
+
+RATE     = int(44100)
+CHUNK    = int(1024)
+CHANNELS = int(1)
+NUM_CHUNKS = 10
+ind = 0
+proc_ind = 2
+len_window = 4096 #Specified length of analysis window
+hop_length_ = 1024 #Specified percentage hop length between windows
+xfade_in = np.linspace(0,1,num=CHUNK)
+xfade_out = np.flip(xfade_in)
+
+class Application(Frame):
+    global make_sine2
+    def make_sine2(seg_length,ii):
+        global track1
+        global track2 
+        global CHUNK
+        global encoder
+        global full_net
+        global full_net_graph
+        global scales  
+        global app 
+        global num_latents
+        global new_data
+        global make_audio
+        global alpha
+        global len_window
+        global tick 
+        num_samps = seg_length*CHUNK
+        make_audio = False
+        num_samps = seg_length*CHUNK
+
+        mysnip1 = track1[(num_samps*ii):(num_samps*(ii+1)+4*CHUNK)] #Snip Track 1
+        mysnip2 = track2[(num_samps*ii):(num_samps*(ii+1)+4*CHUNK)] #Snip Track 2         
+
+        _,_,A = signal.stft(mysnip1, nperseg=len_window, nfft=len_window, fs=44100, noverlap=3*1024) #STFT
+        mag = A 
+        mag = np.abs(mag) #Magnitude response of the STFT
+        rememberA = mag.max(axis=0)+0.000000001 #Used for normalizing STFT frames (with addition to avoid division by zero)
+        magA = mag / rememberA #Normalizing
+        phaseA = np.angle(A) #Phase response of STFT
+        magA = magA.T
+
+        _,_,B = signal.stft(mysnip2, nperseg=len_window, nfft=len_window, fs=44100, noverlap=3*1024) #STFT
+        mag = B 
+        mag = np.abs(mag) #Magnitude response of the STFT
+        rememberB = mag.max(axis=0)+0.000000001 #Used for normalizing STFT frames (with addition to avoid division by zero)
+        magB = mag / rememberB #Normalizing
+        phaseB = np.angle(B) #Phase response of STFT
+        magB = magB.T
+
+        temp_alpha = np.tile(alpha*scales,(NUM_CHUNKS+5,1)) #Match dims for XFade 1
+        temp_negalpha = np.tile((1-alpha)*scales,(NUM_CHUNKS+5,1)) #Match dims for XFade 2
+        temp_phase =alpha*phaseA+(1-alpha)*phaseB #Unstack and Interpolate Phase
+        temp_remember = alpha*rememberA+(1-alpha)*rememberB #Unstack and Interpolate Normalizing gains
+        temp_out_mag = full_net.predict([magA,magB,temp_alpha,temp_negalpha])
+
+        out_mag = temp_out_mag.T * temp_remember
+        E = out_mag*np.exp(1j*temp_phase)
+        _, now_out = np.float32(signal.istft(0.24*E, fs=44100, noverlap=3*1024))
+        out = now_out[CHUNK:-2*CHUNK]
+        new_data = out.reshape(((len(out)/CHUNK),CHUNK))
+
+    global callback 
+    def callback(in_data, frame_count, time_info, status):
+        global ind
+        global proc_ind 
+        global NUM_CHUNKS
+        global all_data
+        global new_data
+        global make_audio
+        global tick 
+        global all_data
+
+        if ind>=NUM_CHUNKS:
+            ind = 0
+            proc_ind+=1
+        if ind==0:
+            xfade = xfade_in*new_data[0,:] + xfade_out*all_data[-1,:]
+            all_data = new_data
+            all_data[0,:] = xfade
+        if ind==1:
+            make_audio = True
+            tick = time.time()
+
+        data = all_data[ind,:] #Send a chunk to the audio buffer when it asks for one
+        ind +=1 
+        return (data, pyaudio.paContinue)  
+
+    def render(self):
+        global mag1
+        global phase1 
+        global remember1
+        global mag2
+        global phase2 
+        global remember2
+        global CHUNK
+        global encoder
+        global enc_graph
+        global decoder 
+        global dec_graph
+        global scales  
+        global app 
+
+        print(scales)
+        temp_out_mag = mag1
+        temp_phase = phase1
+        temp_remember = remember1
+
+        with enc_graph.as_default():
+            temp_enc_mag = encoder.predict(temp_out_mag)
+            enc_mag = temp_enc_mag * scales #NEED TO ADD SCALE HERE
+        with dec_graph.as_default():
+            temp_out_mag = decoder.predict(enc_mag)
+
+        out_mag = temp_out_mag.T * temp_remember
+        E = out_mag*np.exp(1j*temp_phase)
+        out = np.float32(librosa.istft(E))
+        out = (0.9/np.max(np.abs(out)))*out
+
+        sf.write('rendered.wav', out, 44100, subtype='PCM_16')
+        print('done rendering')
+
+
+    def record(self):
+        global mag
+        global phase 
+        global remember
+        global CHUNK
+        global encoder
+        global enc_graph
+        global decoder 
+        global dec_graph
+        global scales  
+        global app 
+        global recorded_scales
+        global proc_ind
+
+        first_ind = 0
+        last_ind = 0
+        total_frames = 0
+        if self.RECORD_var.get() == 1:
+            first_ind = proc_ind
+            print('Button On')
+            self.start_net()
+        else:
+            last_ind = proc_ind
+            print('Button off')
+            self.pause_sounds()
+
+            total_frames = (last_ind-first_ind)*NUM_CHUNKS
+            out_scales = np.ones((total_frames,15))
+            temp_scales = np.vstack(recorded_scales)
+            a = temp_scales.shape[0]
+            increase_by = total_frames//a+1
+            kurt=0
+            for ii in range(a):
+                the_rows = np.arange((kurt*increase_by),min(((kurt+1)*increase_by),total_frames))
+                out_scales[the_rows,:] = np.tile(temp_scales[ii,:],(len(the_rows),1))
+                kurt+=1
+            ind_array = np.arange((first_ind),(NUM_CHUNKS*(last_ind)))
+            temp_out_mag = mag[ind_array,:]
+            temp_phase = phase[:,ind_array]
+            temp_remember = remember[ind_array]
+
+            with enc_graph.as_default():
+                temp_enc_mag = encoder.predict(temp_out_mag)
+                enc_mag = temp_enc_mag * out_scales #NEED TO ADD SCALE HERE
+            with dec_graph.as_default():
+                temp_out_mag = decoder.predict(enc_mag)
+
+            out_mag = temp_out_mag.T * temp_remember
+            E = out_mag*np.exp(1j*temp_phase)
+            out = np.float32(librosa.istft(E))
+            out = (0.9/np.max(np.abs(out)))*out
+
+            sf.write('recorded.wav', out, 44100, subtype='PCM_16')
+            print('done recording')
+
+
+
+    def model_to_mem(self):
+        global encoder
+        global enc_graph
+        global decoder 
+        global dec_graph
+        global full_net
+        global full_net_graph
+
+        data_path_net = os.path.join(os.getcwd(),self.model_name.get()+'_trained_bass.h5')
+        full_net = load_model(data_path_net, compile=False)
+        full_net._make_predict_function()
+        full_net_graph = tf.get_default_graph()
+
+
+    def process_track2(self):
+        global mag1
+        global phase1
+        global remember1
+        global mag2
+        global phase2
+        global remember2
+        global track1
+        global track2 
+
+        len_window = 4096 #Specified length of analysis window
+        hop_length_ = 1024 #Specified percentage hop length between windows
+
+        filename_in = self.track1_name.get()
+        data_path = os.path.join(os.getcwd(),filename_in)
+        track1, _ = librosa.load(data_path, sr=44100, mono=True)
+
+        filename_in = self.track2_name.get()
+        data_path = os.path.join(os.getcwd(),filename_in)
+        track2, _ = librosa.load(data_path, sr=44100, mono=True)
+
+        print('tracks loaded')
+
+
+    def start_net(self):
+        global p 
+        global stream
+        global make_audio
+        global NUM_CHUNKS
+        global proc_ind
+        global tick 
+
+        tick = time.time()
+
+        self.model_to_mem()
+
+        make_audio = True
+        make_sine2(NUM_CHUNKS,1)
+
+        p = pyaudio.PyAudio()
+        print("opening stream")
+        stream = p.open(format=pyaudio.paFloat32,
+                        channels=CHANNELS,
+                        frames_per_buffer=CHUNK,
+                        rate=RATE,
+                        output=True,
+                        stream_callback=callback)
+
+
+        stream.start_stream()
+        time.sleep(0.1)
+
+    def pause_sounds(self):
+        global p 
+        global stream
+        global ind
+        global proc_ind 
+        
+        stream.stop_stream()
+        print('sounds paused')
+        stream.close()
+        p.terminate()
+        ind = NUM_CHUNKS+1
+        proc_ind = 0
+
+    def quit(self):
+        root.destroy()
+        
+
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"]   = "red"
+        self.QUIT["command"] =  self.quit
+        self.QUIT.pack()
+        self.QUIT.place(relx=0.45,rely=0.95)
+
+        self.model_name = Entry(self)
+        self.model_name.pack()
+        self.model_name.place(relx=0.4,rely=0.65)
+        self.label = Label(self,text='Model Name')
+        self.label.pack()
+        self.label.place(relx=0.25,rely=0.65)
+
+        self.track1_name = Entry(self)
+        self.track1_name.pack()
+        self.track1_name.place(relx=0.20,rely=0.6)
+        self.label_1 = Label(self,text='Track 1')
+        self.label_1.pack()
+        self.label_1.place(relx=0.14,rely=0.6)
+
+        self.track2_name = Entry(self)
+        self.track2_name.pack()
+        self.track2_name.place(relx=0.59,rely=0.6)
+        self.label_2 = Label(self,text='Track 2')
+        self.label_2.pack()
+        self.label_2.place(relx=0.53,rely=0.6)
+
+        self.START = Button(self)
+        self.START["text"] = "START"
+        self.START["fg"]   = "green"
+        self.START["command"] =  lambda: self.start_net()
+        self.START.pack()
+        self.START.place(relx=0.45,rely=0.9)
+
+        self.PAUSE = Button(self)
+        self.PAUSE["text"] = "PAUSE"
+        self.PAUSE["fg"]   = "black"
+        self.PAUSE["command"] =  lambda: self.pause_sounds()
+        self.PAUSE.pack()
+        self.PAUSE.place(relx=0.45,rely=0.85)
+
+        self.RECORD_var = IntVar()
+        self.RECORD = Checkbutton(self, variable=self.RECORD_var)
+        self.RECORD["text"] = "RECORD"
+        self.RECORD["fg"]   = "black"
+        self.RECORD["command"] =  lambda: self.record()
+        self.RECORD.pack()
+        self.RECORD.place(relx=0.45,rely=0.8)
+
+        self.RENDER = Button(self)
+        self.RENDER["text"] = "RENDER"
+        self.RENDER["fg"]   = "black"
+        self.RENDER["command"] =  lambda: self.render()
+        self.RENDER.pack()
+        self.RENDER.place(relx=0.45,rely=0.75)
+
+        self.LOAD = Button(self)
+        self.LOAD["text"] = "LOAD TRACKS"
+        self.LOAD["fg"]   = "black"
+        self.LOAD["command"] =  lambda: self.process_track2()
+        self.LOAD.pack()
+        self.LOAD.place(relx=0.45,rely=0.7)
+
+        self.FADE = Scale(self,from_=100, to=0,length=300, orient='horizontal')
+        self.FADE.set(50)
+        self.FADE.pack()
+        self.FADE.place(relx=0.30,rely=0.54)
+
+
+
+    def createSliders(self):
+        global scales 
+        global num_latents
+        scales = np.ones(num_latents)
+        self.scale_list = []
+        for w in range(num_latents):
+            scale = Scale(self,from_=200, to=0,length=200)
+            scale.pack()
+            scale.place(relx=w/float(num_latents),rely=0.2)
+            scale.set(100)
+            scales[w]=scale.get()
+            self.scale_list.append(scale)
+
+    def update_scales(self):
+        global scales 
+        global recorded_scales
+        global make_audio
+        global NUM_CHUNKS
+        global proc_ind
+        global alpha
+        global temp_scales
+
+        alpha = self.FADE.get()/100.
+        if make_audio:
+            t = time.time()
+            make_sine2(NUM_CHUNKS,proc_ind)
+
+
+        for w in range(num_latents):
+            temp_scales[w]=self.scale_list[w].get()/100.
+        scales = temp_scales
+        if self.RECORD_var.get() == 1:
+            recorded_scales.append(scales)
+        self.after(POLL_TIME, self.update_scales)
+
+
+    def __init__(self, master=None):
+        global recorded_scales
+        global POLL_TIME
+        global make_audio
+        global alpha
+        global temp_scales
+        global all_data
+
+        alpha = 1
+        temp_scales = np.ones(num_latents)
+        all_data = np.zeros((21,1024))
+
+        make_audio = False
+        POLL_TIME = 1
+
+        Frame.__init__(self, master,width=800, height=800)
+        self.pack()
+        self.createWidgets()
+        self.createSliders()
+        recorded_scales = []
+        self.update_scales()
+
+global app 
+global num_latents
+num_latents = int(sys.argv[1])
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
---
+++ train-network.py
@@ -0,0 +1,393 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf 
+import keras
+from keras.layers import Input, Dense, Lambda, Concatenate, Dropout, LeakyReLU, Multiply, Add
+from keras.models import Model, Sequential, load_model, clone_model
+from keras.activations import linear
+
+from keras.regularizers import l2
+from keras.losses import mse
+from keras.callbacks import LambdaCallback
+from keras.optimizers import Adam
+
+from keras import backend as K
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse
+import os
+import librosa
+
+
+global alpha
+global beta
+beta = K.variable(3e-7)
+alpha = K.variable(0.3)
+np.random.seed(90210)
+
+def change_params(epoch, logs):
+	if epoch<=5 and epoch%1==0:
+		K.set_value(beta,K.get_value(beta)+2e-5)
+	if epoch == 30:
+		K.set_value(alpha,0.0)
+
+def get_arguments():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--filename_in_1', type=str)
+	parser.add_argument('--filename_out', type=str)
+	parser.add_argument('--net_type', type=str)
+	parser.add_argument('--mode', type=str)
+	parser.add_argument('--trained_model_name', type=str, default='')
+	parser.add_argument('--n_epochs', type=int, default=5)
+	parser.add_argument('--skip', type=bool, default=False)
+	return parser.parse_args()
+
+class Manne:
+	def __init__(self, args):
+		self.frames = []
+		self.X_train = []
+		self.X_val = []
+		self.X_test = []
+		self.encoder = []
+		self.decoder = []
+		self.network = []
+		self.dnb_net = []
+		self.encoder_widths = []
+		self.decoder_widths = []
+
+		self.z_mean = K.placeholder(shape=(8,))
+		self.z_log_var = K.placeholder(shape=(8,))
+		self.beta_changer = []
+
+		self.n_epochs = args.n_epochs
+		self.net_type = args.net_type
+		self.skip = args.skip
+		self.filename_in_1 = args.filename_in_1
+		# self.filename_in_2 = args.filename_in_2
+		self.filename_out = args.filename_out
+		self.trained_model_name = args.trained_model_name
+
+	def do_everything(self):
+		self.load_dataset()
+		self.define_net()
+		self.train_net()
+		self.evaluate_net()
+
+	def just_plot(self):
+		self.load_dataset()
+		self.load_net()
+		self.make_net()
+		adam_rate = 5e-4
+		self.network.compile(optimizer=Adam(lr=adam_rate), loss=self.my_mse, metrics=[self.my_mse])
+		self.evaluate_net()
+		self.save_latents()
+
+	def sampling(self,args):
+		self.z_mean, self.z_log_var = args
+		batch = K.shape(self.z_mean)[0]
+		dim = K.int_shape(self.z_mean)[1]
+		epsilon = K.random_normal(shape=(batch,dim))
+		return self.z_mean + K.exp(0.5*self.z_log_var)*epsilon
+
+	def get_loss(self, inputs, outputs):
+		global beta
+		reconstruction_loss = mse(inputs[:,:2049],outputs)
+		kl_loss = 1+self.z_log_var-K.square(self.z_mean)-K.exp(self.z_log_var)
+		kl_loss = K.sum(kl_loss, axis=-1)
+		kl_loss *= -0.5*beta
+		vae_loss = K.sum(reconstruction_loss+kl_loss)
+		return vae_loss
+
+	def my_mse(self, inputs, outputs):
+		return mse(inputs[:,:4098],outputs)
+
+	def my_mse2(self, inputs, outputs):
+		inspec1, outspec1 = inputs[0], outputs[0]
+		inspec2, outspec2 = inputs[1], outputs[1]
+		outloss = K.sum(mse(inspec1,outspec1)+mse(inspec2,outspec2))
+		return outloss
+
+	def my_kl(self, inputs, outputs):
+		kl_loss = 1+self.z_log_var-K.square(self.z_mean)-K.exp(self.z_log_var)
+		kl_loss = K.sum(kl_loss, axis=-1)
+		kl_loss *= -0.5
+		return kl_loss
+
+	def load_net(self):
+		enc_filename = os.path.join(os.getcwd(),'models/'+self.trained_model_name+'_trained_encoder.h5')
+		print(enc_filename)
+		self.encoder = load_model(enc_filename,custom_objects={'sampling': self.sampling}, compile=False)
+		dec_filename = os.path.join(os.getcwd(),'models/'+self.trained_model_name+'_trained_decoder.h5')
+		self.decoder = load_model(dec_filename,custom_objects={'sampling': self.sampling}, compile=False)
+
+	def load_dataset(self):
+		filename = 'frames/'+self.filename_in_1+'_frames.npy'	#Static Data used for training net
+		filepath = os.path.join(os.getcwd(),filename)
+		orig_frames = np.load(filepath)
+		orig_frames_1 = np.asarray(orig_frames)
+		np.random.shuffle(orig_frames_1)
+		orig_frames_1 = orig_frames_1[:50000,:]
+		len_frames = orig_frames_1.shape[0]
+
+
+		self.frames = orig_frames_1
+		orig_frames = None
+		orig_frames_1 = None
+
+
+		if args.filename_in_1 == 'one_octave':
+			self.X_train = self.frames[:16685,:]
+			self.X_val = self.frames[16685:17998,:]
+			self.X_test = self.frames[17998:,:]
+		elif args.filename_in_1 == 'five_octave':
+			self.X_train = self.frames[:78991,:]
+			self.X_val = self.frames[78991:84712,:]
+			self.X_test = self.frames[84712:,:]
+		elif args.filename_in_1 == 'guitar':
+			self.X_train = self.frames[:62018,:]
+			self.X_val = self.frames[62018:66835,:]
+			self.X_test = self.frames[66835:,:]
+		elif args.filename_in_1 == 'violin':
+			self.X_train = self.frames[:90571,:]
+			self.X_val = self.frames[90571:100912,:]
+			self.X_test = self.frames[100912:,:]
+		else:
+			in_size = int(self.frames.shape[0])
+			self.X_train =  self.frames[:int(0.95*in_size),:] 
+			self.X_val =  self.frames[int(0.95*in_size):int(0.975*in_size),:]
+			self.X_test =  self.frames[int(0.975*in_size):,:]
+
+
+
+	def define_net(self):
+		global decoder_outdim
+		if self.net_type=='vae':
+			l2_penalty = 0
+		else:
+			l2_penalty = 1e-7
+
+		self.encoder_widths = [256,128,64,32,16,15]
+		self.decoder_widths = [16,32,64,128,256]
+
+
+		decoder_outdim = 2049
+		drop = 0.0
+		alpha_val=0.1
+		input_spec1 = Input(shape=(decoder_outdim,))
+
+		encoded1 = Dense(units=self.encoder_widths[0],
+				activation=None,
+				kernel_regularizer=l2(l2_penalty))(input_spec1)
+
+		encoded1 = LeakyReLU(alpha=alpha_val)(encoded1)
+		for width in self.encoder_widths[1:-1]:
+			encoded1 = Dense(units=width,
+				activation=None,
+				kernel_regularizer=l2(l2_penalty))(encoded1)
+			encoded1 = LeakyReLU(alpha=alpha_val)(encoded1)
+
+		encoded1 = Dense(units=self.encoder_widths[-1], activation='sigmoid', kernel_regularizer=l2(l2_penalty))(encoded1)
+
+
+		self.encoder = Model(inputs=[input_spec1], outputs=[encoded1])
+
+		if self.net_type == 'vae':
+			self.z_mean = Dense(self.encoder_widths[-1],input_shape=(self.encoder_widths[-1],), name='z_mean')(encoded)
+			self.z_log_var = Dense(self.encoder_widths[-1],input_shape=(self.encoder_widths[-1],), name='z_log_var')(encoded)
+			z = Lambda(self.sampling,output_shape=(self.encoder_widths[-1],), name='z')([self.z_mean,self.z_log_var])
+			self.encoder = Model(input_spec, [self.z_mean, self.z_log_var, z])
+		else:
+			a = 1
+
+		if self.skip == True:
+			input_latent1 = Input(shape=(self.encoder_widths[-1],))
+		else:
+			input_latent = Input(shape=(self.encoder_widths[-1]*2,))
+
+
+		decoded1 = Dense(units=self.decoder_widths[0],
+			activation=None,
+			kernel_regularizer=l2(l2_penalty))(input_latent1)
+		decoded1 = LeakyReLU(alpha=alpha_val)(decoded1)
+		for width in self.decoder_widths[1:]:
+			decoded1 = Dense(units=width,
+				activation=None,
+				kernel_regularizer=l2(l2_penalty))(decoded1)
+			decoded1 = LeakyReLU(alpha=alpha_val)(decoded1)
+
+		decoded1 = Dense(units=int(decoder_outdim),
+			activation='relu',
+			kernel_regularizer=l2(l2_penalty))(decoded1)
+
+
+		self.decoder = Model(inputs=[input_latent1], outputs=[decoded1])
+
+		my_batch = [input_spec1]
+		my_encoded = self.encoder(my_batch)
+		my_decoded = self.decoder(my_encoded)
+		self.network = Model(inputs=my_batch, outputs=my_decoded)
+
+		print('\n net summary \n')
+		self.network.summary()
+		print('\n enc summary \n')
+		self.encoder.summary()
+		print('\n dec summary \n')
+		self.decoder.summary()
+
+
+
+	def train_net(self):
+		global decoder_outdim
+		adam_rate = 1e-4
+		if self.skip == True: #Handling case where Keras expects two inputs
+			train_data = [self.X_train[:,:2049]]
+			train_target = [self.X_train[:,:2049]]
+			val_data = [self.X_val[:,:2049]]
+			val_target = [self.X_val[:,:2049]]
+		else:
+			train_data = self.X_train
+			val_data = self.X_val
+		if self.net_type == 'vae':
+			beta_changer = LambdaCallback(on_epoch_end=change_params)
+			self.network.compile(optimizer=Adam(lr=adam_rate), loss=self.get_loss, metrics=[self.my_mse, self.my_kl])
+			self.network.fit(x=train_data, y=self.X_train,
+					epochs=self.n_epochs,
+					batch_size=200,
+					shuffle=True,
+					validation_data=(val_data, self.X_val),
+					callbacks=[beta_changer]
+					)
+
+		else:
+			alpha_changer = LambdaCallback(on_epoch_end=change_params)
+			self.network.compile(optimizer=Adam(lr=adam_rate), loss=mse)
+			self.network.fit(x=train_data, y=train_target,
+					epochs=self.n_epochs,
+					batch_size=200,
+					shuffle=True,
+					validation_data=(val_data, val_target),
+					callbacks=[alpha_changer]
+					)
+
+
+		modalpha1 = Input(shape=(self.encoder_widths[-1],))
+		modnegalpha1 = Input(shape=(self.encoder_widths[-1],))
+		final_spec_1 = Input(shape=(decoder_outdim,))
+		final_spec_2 = Input(shape=(decoder_outdim,))
+		my_batch0 = [final_spec_1]
+		my_batch1 = [final_spec_2]
+		my_encoded0 = self.encoder(my_batch0)
+		my_encoded1 = self.encoder(my_batch1)
+		blarg0 = Multiply()([my_encoded0,modalpha1])
+		blarg1 = Multiply()([my_encoded1,modnegalpha1])
+		mod_latent1 = Add()([blarg0,blarg1])
+		final_decoded = self.decoder([mod_latent1])
+		self.dnb_net = Model(inputs=[final_spec_1,final_spec_2,modalpha1,modnegalpha1],
+			outputs=final_decoded)
+
+
+		self.dnb_net.save('models/'+self.net_type+'_'+self.filename_out+'_trained_network.h5')
+
+	def save_latents(self):
+
+		indat = self.frames
+		enc_mag = self.encoder.predict(indat,verbose=1)
+
+		if self.net_type == 'vae':
+			a = enc_mag[0]
+			b = enc_mag[1]
+			print(a.shape)
+			print(b.shape)
+			enc_mag = np.hstack((enc_mag[0],enc_mag[1]))
+
+		df = pd.DataFrame(enc_mag)
+		df.to_csv('encoded_mags.csv')
+
+
+	def evaluate_net(self):
+		if self.skip == True: #Handling case where Keras expects two inputs
+			test_data = self.X_test[:,:2049]
+			test_target = self.X_test[:,:2049]
+			val_data = self.X_val[:,:2049]
+			val_target = self.X_val[:,:2049]
+		else:
+			test_data = self.X_test
+			val_data = self.X_val
+
+		if args.filename_in_1 == 'one_octave':
+			mod = 1
+		elif args.filename_in_1 == 'five_octave' or args.filename_in_1 == 'violin':
+			mod = 10
+		elif args.filename_in_1 == 'guitar':
+			mod = 3
+		else:
+			mod = 1
+
+		print('\n')
+		print('Evaluating performance on validation and test sets')
+		a=self.network.evaluate(x=val_data,y=val_target,verbose=1)
+		b=self.network.evaluate(x=test_data,y=test_target,verbose=1)
+		print('\n')
+		print('Plotting network reconstructions')
+		valset_eval = self.network.predict(val_data,verbose=1)
+		testset_eval = self.network.predict(test_data,verbose=1)
+		frame_check = [10, 15, 20, 25, 30]#, 350, 400, 450, 500]
+
+		for frame in frame_check:
+			frame *= mod
+			xx = np.arange(2049)*(22050/2049)
+			val_yy = val_target[frame,0:2049]
+			val_zz = valset_eval[frame,0:2049]
+			test_yy = test_target[frame,0:2049]
+			test_zz = testset_eval[frame,0:2049]
+			plt.figure(1)
+			plt.subplot(211)
+			plt.plot(xx,val_yy)
+			plt.ylim([0,1.2])
+			plt.ylabel('Spectral Magnitude')
+			plt.xscale('log')
+			plt.xlabel('Frequency (Hz)')
+			plt.title('Input Spectrum')
+			plt.subplot(212)
+			plt.plot(xx,val_zz,color='r')
+			plt.ylim([0,1.2])
+			plt.ylabel('Spectral Magnitude')
+			plt.xscale('log')
+			plt.xlabel('Frequency (Hz)')
+			plt.title('Output Spectrum')
+			plt.tight_layout()
+			plotname = self.net_type+'_drums_'+str(frame)+'.pdf'
+			plt.savefig(plotname, format = 'pdf', bbox_inches='tight')
+			plt.clf()
+
+			plt.figure(1)
+			plt.subplot(211)
+			plt.plot(xx,test_yy)
+			plt.ylim([0,1.2])
+			plt.ylabel('Spectral Magnitude')
+			plt.xscale('log')
+			plt.xlabel('Frequency (Hz)')
+			plt.title('Input Spectrum')
+			plt.subplot(212)
+			plt.plot(xx,test_zz,color='r')
+			plt.ylim([0,1.2])
+			plt.ylabel('Spectral Magnitude')
+			plt.xscale('log')
+			plt.xlabel('Frequency (Hz)')
+			plt.title('Output Spectrum')
+			plt.tight_layout()
+			plotname = self.net_type+'_bass_'+str(frame)+'.pdf'
+			plt.savefig(plotname, format = 'pdf', bbox_inches='tight')
+			plt.clf()
+
+
+if __name__ == '__main__':
+	args = get_arguments()
+	my_manne = Manne(args)
+	if args.mode == 'train':
+		my_manne.do_everything()
+	else:
+		my_manne.just_plot()
